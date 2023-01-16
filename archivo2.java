ff21ebb915b8923b39b21c2f4ce4294ff622cb0c
Alan Hernandez <alanjavier852@gmail.com>
2023-01-16 15:00:46 -0600

    Actualización archivo1

--- archivo2.java
+++ archivo2.java
@@ -1,5 +1,8 @@
 public class archivo2 {
     public static void main(String[] args) {
+        System.out.println("Ete shec");
         System.out.println("Otro puto chanchito feliz ");
+        System.out.println("El pepe ");
+        System.out.println("Perra bomba madre");
     }
 }
